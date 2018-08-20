3385b968b9090f8ff9e32951999a8945f44a4418
zwq-qianyu <1347638091@qq.com>
2018-08-20 09:05:50 +0800

    模型改进
    之前模拟的本地数据都是用数组的形式，现在改进成用结构体类

---
+++ iOS中级/1、模型改进--类与结构体/阶段型结果/Weapons/WeaponDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeaponDetailViewController.swift
+//  Weapons
+//
+//  Created by 梓逸宸 on 2018/8/20.
+//  Copyright © 2018年 梓逸宸. All rights reserved.
+//
+
+import UIKit
+
+class WeaponDetailViewController: UIViewController {
+    var imageName = ""
+    
+    @IBOutlet weak var headerImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(imageName)
+        
+        headerImageView.image = UIImage(named: imageName)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
