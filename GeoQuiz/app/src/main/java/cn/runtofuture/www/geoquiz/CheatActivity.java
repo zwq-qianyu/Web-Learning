8532bb20e27aeddaa676cdb3625d905d0d618918
zwq-qianyu <1347638091@qq.com>
2018-10-08 01:03:08 +0800

    更新至第5章  接下来该第6章了

---
+++ GeoQuiz/app/src/main/java/cn/runtofuture/www/geoquiz/CheatActivity.java
@@ -0,0 +1,58 @@
+package cn.runtofuture.www.geoquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends AppCompatActivity {
+    public static final String EXTRA_ANSWER_IS_TRUE = "cn.runtofuture.www.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOW = "cn.runtofuture.www.geoquiz.answer_shown";
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerTextView = (TextView)findViewById(R.id.answer_text_view);
+
+        mShowAnswerButton = (Button)findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                }
+                else{
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShowResult(true);
+            }
+        });
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+    }
+
+    private void setAnswerShowResult(boolean isAnswerShown){
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOW, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    public static boolean wasAnswerShown(Intent result){
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOW, false);
+    }
+
+}
--- GeoQuiz/app/src/main/java/cn/runtofuture/www/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/cn/runtofuture/www/geoquiz/QuizActivity.java
@@ -1,7 +1,10 @@
 package cn.runtofuture.www.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +14,13 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private Button mNextButton;
+
     private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -23,12 +31,17 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true)
     };
 
-    private int currentIndex = 0;
+    private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
@@ -49,32 +62,104 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
+            // 跳转到 CheatActivity
+            @Override
+            public void onClick(View v){
+//                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+
+
         mNextButton = (Button)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentIndex  = (currentIndex + 1) % mQuestionBank.length;
+                mCurrentIndex  = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateTextView();
             }
         });
         updateTextView();
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resulCode, Intent data){
+        if(resulCode != Activity.RESULT_OK){
+            return;
+        }
+
+        if(requestCode == REQUEST_CODE_CHEAT){
+            if(data == null){
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()  called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    // 覆盖onSaveInstanceState函数，保存mCurrentIndex的值，使得在横屏后不会问题又回到第一个
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    // 更新问题
     private void updateTextView(){
-        int question = mQuestionBank[currentIndex].getTextResid();
+        int question = mQuestionBank[mCurrentIndex].getTextResid();
         mQuestionTextView.setText(question);
     }
 
+    // 判断答案是否正确
     private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mQuestionBank[currentIndex].isAnswerTrue();
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
         int messageResid = 0;
 
-        if(answerIsTrue == userPressedTrue) {
-            messageResid = R.string.correct_toast;
-        }
-        else{
-            messageResid = R.string.incorrect_toast;
+        if(mIsCheater) {
+            messageResid = R.string.judgment_toast;
+        }else{
+            if(answerIsTrue == userPressedTrue) {
+                messageResid = R.string.correct_toast;
+            }
+            else{
+                messageResid = R.string.incorrect_toast;
+            }
         }
 
         Toast.makeText(this, messageResid, Toast.LENGTH_SHORT).show();
